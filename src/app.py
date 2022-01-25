b7c9098d7a52459deee5a21f9b5e6a742bf7fa6f
afunderburg <afunderburg@connectwise.com>
2022-01-25 14:50:45 -0500

    Add github workflows for deployment

--- src/app.py
+++ src/app.py
@@ -49,7 +49,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "First GitHub Deployment successful",
             # "location": ip.text.replace("\n", "")
         }),
     }
